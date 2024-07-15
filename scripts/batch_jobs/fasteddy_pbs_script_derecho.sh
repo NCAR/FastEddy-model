d4b79fb694754e498e14922567973187ef81b899
Julie Prestopnik <jpresto@ucar.edu>
2024-07-15 14:53:45 -0600

    Per #42, adding initial batch scripts

---
+++ scripts/batch_jobs/fasteddy_pbs_script_derecho.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -A <ProjectAccount>
+#PBS -N FastEddy 
+#PBS -l select=1:ncpus=4:mpiprocs=4:ngpus=4:mem=100GB
+#PBS -l walltime=12:00:00
+#PBS -q main 
+#PBS -r n 
+#PBS -j oe
+#PBS -l job_priority=economy
+
+export BASEDIR=<path to code location>/FastEddy-model/
+export SRCDIR=${BASEDIR}/SRC/FEMAIN
+export TUTORIALDIR=${BASEDIR}/tutorials/
+export EXAMPLE=<example .in filename>
+
+hostname
+module -t list
+echo " "
+
+mpiexec -n 4 --ppn 4 set_gpu_rank ${SRCDIR}/FastEddy ${TUTORIALDIR}/examples/${EXAMPLE}
