d7f856de7f64a45bbad2ef0fe9241c0209776a71
domingom <domingom@ucar.edu>
2025-02-14 18:20:03 -0700

    python coupling tools (2/3): SimGrid

---
+++ scripts/python_utilities/coupler/SimGrid.py
@@ -0,0 +1,356 @@
+import os, sys
+import numpy as np
+import numpy.matlib
+import xarray as xr
+import argparse
+import json
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import time
+import matplotlib
+import math
+import struct
+from scipy.interpolate import RectBivariateSpline, NearestNDInterpolator
+from couplingUtils import *
+
+#######################################
+### read parameters from .json file ###
+#######################################
+
+args = parse_args()
+with open(args.file) as file:
+    params = json.loads(file.read())
+#
+name_dom = params["name_dom"]
+FE_ref_GIS_nc = params["FE_ref_GIS_nc"]
+FE_params_file = params["FE_params_file"]
+center_lat = params["center_lat"]
+center_lon = params["center_lon"]
+FE_new_nc_path = params["FE_new_nc_path"]
+name_dom_add = params["name_dom_add"]
+save_plot_opt = params["save_plot_opt"]
+
+#######################################
+
+# Read in FE params file
+
+FE_params = get_params_FE(FE_params_file)
+
+# Other steps
+
+start_code_all = time.perf_counter()
+
+FE_new_nc = FE_new_nc_path + name_dom + name_dom_add + '.0'
+FE_plot = FE_new_nc_path + name_dom + name_dom_add + '.png'
+
+print('FE_ref_GIS_nc: ',FE_ref_GIS_nc)
+print('FE_new_nc: ',FE_new_nc)
+
+if not os.path.exists(FE_new_nc_path):
+    os.makedirs(FE_new_nc_path)
+
+ds_GIS = xr.open_dataset(FE_ref_GIS_nc)
+
+dx_inter = ds_GIS.dx_inter.values
+dy_inter = ds_GIS.dy_inter.values
+
+Nz = int(str(FE_params['Nz'][0]))
+
+### determine x_s,x_e,y_s,y_e for the coarsened grid ###
+
+xPos_2d = ds_GIS.xPos2d.values
+yPos_2d = ds_GIS.yPos2d.values
+lat = ds_GIS.lat.values
+lon = ds_GIS.lon.values
+
+Nx = int(str(FE_params['Nx'][0]))
+Ny = int(str(FE_params['Ny'][0]))
+d_xi = float(str(FE_params['d_xi'][0]))
+d_eta = float(str(FE_params['d_eta'][0]))
+d_zeta = float(str(FE_params['d_zeta'][0]))
+
+print('Nx,Ny,d_xi,d_eta,d_zeta=',Nx,',',Ny,',',d_xi,',',d_eta,',',d_zeta)
+
+# old code
+#r_diss = np.sqrt(np.power(xPos_2d-bdy_x,2.0)+np.power(yPos_2d-bdy_y,2.0))
+#ind_closest = np.where(r_diss==np.amin(np.amin(r_diss)))
+#print('ind_closest=',ind_closest)
+#print('xPos_2d[ind_closest]=',xPos_2d[ind_closest])
+#print('yPos_2d[ind_closest]=',yPos_2d[ind_closest])
+#x_ind = ind_closest[1]
+#x_s = x_ind[0]
+#y_ind = ind_closest[0]
+#y_s = y_ind[0]
+#print('xPos_2d[y_s,x_s]=',xPos_2d[y_s,x_s],'m')
+#print('yPos_2d[y_s,x_s]=',yPos_2d[y_s,x_s],'m')
+#print('diff_x=',xPos_2d[y_s,x_s]-bdy_x,'m')
+#print('diff_y=',yPos_2d[y_s,x_s]-bdy_y,'m')
+
+# new code
+ll_diff = np.sqrt(np.power(lat-center_lat,2.0)+np.power(lon-center_lon,2.0))
+ind_center = np.where(ll_diff==np.min(ll_diff,axis=(1,0)))
+ind_cc_y = ind_center[0][0]
+ind_cc_x = ind_center[1][0]
+x_s = ind_cc_x - int(0.5*Nx)
+#Nx_e = Nx_s + Nx
+y_s = ind_cc_y - int(0.5*Ny)
+#Ny_e = Ny_s + Ny
+print('xPos_2d[y_s,x_s]=',xPos_2d[y_s,x_s],'m')
+print('yPos_2d[y_s,x_s]=',yPos_2d[y_s,x_s],'m')
+#print('diff_x=',xPos_2d[y_s,x_s]-bdy_x,'m')
+#print('diff_y=',yPos_2d[y_s,x_s]-bdy_y,'m')
+
+##
+
+npx_inc = int(d_xi/dx_inter)
+npy_inc = int(d_eta/dy_inter)
+if (npx_inc==0):
+    npx_inc = d_xi/dx_inter
+    npy_inc = d_eta/dy_inter
+    x_e = x_s + int(np.ceil(Nx*npx_inc))
+    y_e = y_s + int(np.ceil(Ny*npy_inc))
+else:
+    x_e = x_s + Nx*npx_inc
+    y_e = y_s + Ny*npy_inc
+print('x_s,x_e,y_s,y_e=',x_s,x_e,y_s,y_e)
+
+box_indx = [x_s,x_e,x_e,x_s,x_s]
+box_indy = [y_s,y_s,y_e,y_e,y_s]
+
+x_box_corners = [xPos_2d[box_indy[0],box_indx[0]], xPos_2d[box_indy[1],box_indx[1]], xPos_2d[box_indy[2],box_indx[2]], xPos_2d[box_indy[3],box_indx[3]], xPos_2d[box_indy[4],box_indx[4]]]
+y_box_corners = [yPos_2d[box_indy[0],box_indx[0]], yPos_2d[box_indy[1],box_indx[1]], yPos_2d[box_indy[2],box_indx[2]], yPos_2d[box_indy[3],box_indx[3]], yPos_2d[box_indy[4],box_indx[4]]]
+print('x_box_corners=',x_box_corners,'m')
+print('y_box_corners=',y_box_corners,'m')
+
+# Decide between decimation and interpolation
+if (d_xi%dx_inter==0.0):
+    print('Grid is an even factor of GIS resolution, use decimation')
+    interp_flag = 0
+else:
+    print('Grid is not an even factor of GIS resolution, use interpolation')
+    interp_flag = 1
+print('interp_flag=',interp_flag)
+
+verticalDeformSwitch = int(str(FE_params['verticalDeformSwitch'][0]))
+print('verticalDeformSwitch=',verticalDeformSwitch)
+if (verticalDeformSwitch==1):
+    c1 = float(str(FE_params['verticalDeformFactor'][0]))
+    fCoeff = float(str(FE_params['verticalDeformQuadCoeff'][0]))
+else:
+    c1 = 0.0
+    fCoeff = 0.0
+print('c1,fCoeff=',c1,fCoeff)
+
+######################
+# Calculate new zPos #
+######################
+## Read in terrain elevation array
+topo = ds_GIS.topoPos.values
+if (interp_flag==0):
+    data_topo0 = topo[y_s:y_e:npy_inc,x_s:x_e:npx_inc]
+else:
+    xPos_2d_dom_ori = xPos_2d[y_s:y_e,x_s:x_e]
+    yPos_2d_dom_ori = yPos_2d[y_s:y_e,x_s:x_e]
+    topo_dom_ori = topo[y_s:y_e,x_s:x_e]
+    print('xPos_2d_dom_ori.shape=',xPos_2d_dom_ori.shape)
+    f_topo = RectBivariateSpline(xPos_2d_dom_ori[0,:], yPos_2d_dom_ori[:,0], topo_dom_ori.T, kx=3, ky=3)
+
+    xPos_1d_new = np.arange(x_box_corners[0],x_box_corners[1],d_xi)
+    yPos_1d_new = np.arange(y_box_corners[0],y_box_corners[2],d_eta)
+
+    data_topo0_b = f_topo(xPos_1d_new, yPos_1d_new).T
+    xPos_2d_new_b, yPos_2d_new_b = np.meshgrid(xPos_1d_new, yPos_1d_new)
+
+    data_topo0 = data_topo0_b[0:Ny,0:Nx]
+    xPos_2d_new = xPos_2d_new_b[0:Ny,0:Nx]
+    yPos_2d_new = yPos_2d_new_b[0:Ny,0:Nx]
+
+data_topo = smoothTerrain(data_topo0,d_xi)
+
+topoPos_min = np.min(data_topo,axis=(1,0))
+topoPos_max = np.max(data_topo,axis=(1,0))
+print('topoPos_min=',topoPos_min)
+print('topoPos_max=',topoPos_max)
+
+terrain_filename = "{:s}_Topography_{:d}x{:d}.dat".format(name_dom,Nx,Ny)
+FE_new_terrain = FE_new_nc_path + terrain_filename
+topoflat = bytearray(Nx*Ny*4)
+struct.pack_into('{:d}f'.format(Nx*Ny),topoflat,0,*data_topo.flatten())
+topoFile = open(FE_new_terrain,'wb')
+topoFile.write(struct.pack('i',Nx))
+topoFile.write(struct.pack('i',Ny))
+topoFile.write(topoflat)
+topoFile.close()
+
+ztop = Nz*d_zeta-0.5*d_zeta # Note the rectilinear vertical resolution max
+zPos_str = np.zeros(Nz)
+print(zPos_str.shape)
+zarr = np.zeros((Nz,Ny,Nx),dtype=np.float32)
+
+if(np.mean(topo,axis=(0,1)) == 0.0):
+  zbot = 0.0
+  print('zbot,ztop=',zbot,'m ,',ztop,'m')
+
+  for kk in range(0,Nz):
+    zPos_uni = kk*d_zeta + 0.5*d_zeta
+    zPos_str[kk] = zDeform(zPos_uni,zbot,ztop,c1,fCoeff)
+    if (kk==0):
+        print('kk,zPos_str,dz=',kk,',',zPos_str[kk],', -')
+    else:
+        print('kk,zPos_str,dz=',kk,',',zPos_str[kk],',',zPos_str[kk]-zPos_str[kk-1])
+    zarr[kk,:,:] = zPos_str[kk]
+else:
+  for j in range(Ny):
+    if(j%int(Ny/10)==0):
+      print('{:d}% complete...'.format(10*int(j/int(Ny/10))))
+    for i in range(Nx):
+      for k in range(Nz):
+        zbot = data_topo[j,i]
+        zPos_uni = k*d_zeta + 0.5*d_zeta
+        zPos_str[k] = zDeform(zPos_uni,zbot,ztop,c1,fCoeff)
+        zarr[k,j,i] = zPos_str[k]
+        if (j==0) and (i==0):
+          if (k==0):
+            print('k,zPos_str,dz=',k,',',zPos_str[k],', -')
+          else:
+            print('k,zPos_str,dz=',k,',',zPos_str[k],',',zPos_str[k]-zPos_str[k-1])
+
+
+ind_topomin = np.where(data_topo==topoPos_min)
+z_lowTopo_v = zarr[:,ind_topomin[0],ind_topomin[1]]
+ind_topomax = np.where(data_topo==topoPos_max)
+z_highTopo_v = zarr[:,ind_topomax[0],ind_topomax[1]]
+
+dz_lowTopo_v = z_lowTopo_v[1:Nz]-z_lowTopo_v[0:Nz-1]
+dz_highTopo_v = z_highTopo_v[1:Nz]-z_highTopo_v[0:Nz-1]
+
+print('topoPos_min =',topoPos_min,'m')
+print('topoPos_max =',topoPos_max,'m')
+print('Domain top =',ztop,'m')
+print('Maximum domain vertical extent =',z_lowTopo_v[-1]-z_lowTopo_v[0],'m')
+print('Minimum domain vertical extent =',z_highTopo_v[-1]-z_highTopo_v[0],'m')
+print('dz_lowTopo_v at the surface =',dz_lowTopo_v[0],'m')
+print('dz_highTopo_v at the surface =',dz_highTopo_v[0],'m')
+print('dz_lowTopo_v at the top =',dz_lowTopo_v[-1],'m')
+print('dz_highTopo_v at the top =',dz_highTopo_v[-1],'m')
+
+# xPos, yPos
+xarr = np.zeros((Nz,Ny,Nx),dtype=np.float32)
+if (interp_flag==0):
+    xPos_2d_new = xPos_2d[y_s:y_e:npy_inc,x_s:x_e:npx_inc]
+    xPos_2d_new = xPos_2d_new - xPos_2d_new[0,0] + 0.5*d_xi
+for kk in range(0,Nz):
+   xarr[kk,:,:] = xPos_2d_new
+
+yarr = np.zeros((Nz,Ny,Nx),dtype=np.float32)
+if (interp_flag==0):
+    yPos_2d_new = yPos_2d[y_s:y_e:npy_inc,x_s:x_e:npx_inc]
+    yPos_2d_new = yPos_2d_new - yPos_2d_new[0,0] + 0.5*d_xi
+for kk in range(0,Nz):
+   yarr[kk,:,:] = yPos_2d_new
+
+# Surface static fields
+z0m_field = ds_GIS.z0m.values
+z0t_field = ds_GIS.z0t.values
+SeaMask_field = ds_GIS.SeaMask.values
+land_cover = ds_GIS.LandCover.values
+
+if (interp_flag==0):
+    data_z0m = z0m_field[y_s:y_e:npy_inc,x_s:x_e:npx_inc]
+    data_z0t = z0t_field[y_s:y_e:npy_inc,x_s:x_e:npx_inc]
+    data_SeaMask = SeaMask_field[y_s:y_e:npy_inc,x_s:x_e:npx_inc]
+    data_landc = land_cover[y_s:y_e:npy_inc,x_s:x_e:npx_inc]
+    lat_dom = lat[y_s:y_e:npy_inc,x_s:x_e:npx_inc]
+    lon_dom = lon[y_s:y_e:npy_inc,x_s:x_e:npx_inc]
+else:
+    f_z0m = NearestNDInterpolator(list(zip(xPos_2d_dom_ori.flatten(), yPos_2d_dom_ori.flatten())), z0m_field[y_s:y_e,x_s:x_e].flatten())
+    f_z0t = NearestNDInterpolator(list(zip(xPos_2d_dom_ori.flatten(), yPos_2d_dom_ori.flatten())), z0t_field[y_s:y_e,x_s:x_e].flatten())
+    f_SeaMask = NearestNDInterpolator(list(zip(xPos_2d_dom_ori.flatten(), yPos_2d_dom_ori.flatten())), SeaMask_field[y_s:y_e,x_s:x_e].flatten())
+    f_landc = NearestNDInterpolator(list(zip(xPos_2d_dom_ori.flatten(), yPos_2d_dom_ori.flatten())), land_cover[y_s:y_e,x_s:x_e].flatten())
+    print('xPos_2d_dom_ori[0,:].shape=',xPos_2d_dom_ori[0,:].shape)
+    print('yPos_2d_dom_ori[:,0].shape=',yPos_2d_dom_ori[:,0].shape)
+    print('lat.T.shape=',lat.T.shape)
+    f_lat = RectBivariateSpline(xPos_2d_dom_ori[0,:], yPos_2d_dom_ori[:,0], lat[y_s:y_e,x_s:x_e].T, kx=3, ky=3)
+    f_lon = RectBivariateSpline(xPos_2d_dom_ori[0,:], yPos_2d_dom_ori[:,0], lon[y_s:y_e,x_s:x_e].T, kx=3, ky=3)
+    data_z0m = f_z0m(xPos_2d_new, yPos_2d_new)
+    data_z0t = f_z0t(xPos_2d_new, yPos_2d_new)
+    data_SeaMask = f_SeaMask(xPos_2d_new, yPos_2d_new)
+    data_landc = f_landc(xPos_2d_new, yPos_2d_new)
+    lat_dom_b = f_lat(xPos_1d_new, yPos_1d_new).T
+    lon_dom_b = f_lon(xPos_1d_new, yPos_1d_new).T
+    lat_dom = lat_dom_b[0:Ny,0:Nx]
+    lon_dom = lon_dom_b[0:Ny,0:Nx]
+
+# Save to netCDF file
+
+ds_data = xr.Dataset()
+
+ds_data['xPos']= xr.DataArray(xarr,dims=(['zIndex','yIndex','xIndex']))
+ds_data['yPos']= xr.DataArray(yarr,dims=(['zIndex','yIndex','xIndex']))
+ds_data['zPos']= xr.DataArray(zarr,dims=(['zIndex','yIndex','xIndex']))
+ds_data['topoPos']= xr.DataArray(data_topo.astype(dtype=np.float32),dims=(['yIndex','xIndex']))
+ds_data['z0m']= xr.DataArray(data_z0m.astype(dtype=np.float32),dims=(['yIndex','xIndex']))
+ds_data['z0t']= xr.DataArray(data_z0t.astype(dtype=np.float32),dims=(['yIndex','xIndex']))
+ds_data['SeaMask']= xr.DataArray(data_SeaMask.astype(dtype=np.float32),dims=(['yIndex','xIndex']))
+ds_data['LandCover']= xr.DataArray(data_landc.astype(dtype=np.int32),dims=(['yIndex','xIndex']))
+ds_data['lat']= xr.DataArray(lat_dom.astype(dtype=np.float64),dims=(['yIndex','xIndex']))
+ds_data['lon']= xr.DataArray(lon_dom.astype(dtype=np.float64),dims=(['yIndex','xIndex']))
+
+ds_data.to_netcdf(FE_new_nc,format='NETCDF4')
+
+end_code_all = time.perf_counter()
+print('done creating reference FastEddy grid and surface fields, elapsed time ' + str(end_code_all-start_code_all) + ' s')
+
+# Plot
+
+if (save_plot_opt==1):
+
+    start_code = time.perf_counter()
+
+    fntSize=16
+    fntSize_title=22
+    fntSize_legend=16
+    fntSize_label=16
+    plt.rcParams['xtick.labelsize']=fntSize
+    plt.rcParams['ytick.labelsize']=fntSize
+    plt.rcParams['axes.linewidth']=2.0
+
+    numPlotsX = 1
+    numPlotsY = 2
+
+    sizeX = 10
+    sizeY = 6
+
+    fig,axs = plt.subplots(numPlotsX,numPlotsY,sharey=False,sharex=False,figsize=(sizeX,sizeY))
+
+    ###############
+    ### panel 0 ###
+    ###############
+    ax = axs[0]
+
+    im2 = ax.plot(z_lowTopo_v,'-',color='b',linewidth=2.0,zorder=0,label='lowTopo')
+    im2 = ax.plot(z_highTopo_v,'-',color='r',linewidth=2.0,zorder=0,label='highTopo')
+    ax.set_ylabel(r"$z$ $[$m$]$",fontsize=fntSize)
+    ax.set_xlabel(r"Vertical index $[$-$]$",fontsize=fntSize)
+    ax.legend(loc=0,prop={'size': fntSize_legend},edgecolor='white')
+
+    ###############
+    ### panel 1 ###
+    ###############
+    ax = axs[1]
+
+    im2 = ax.plot(dz_lowTopo_v,'-',color='b',linewidth=2.0,zorder=0)
+    im2 = ax.plot(dz_highTopo_v,'-',color='r',linewidth=2.0,zorder=0)
+    ax.set_ylabel(r"$\Delta z$ $[$m$]$",fontsize=fntSize)
+    ax.set_xlabel(r"Vertical index $[$-$]$",fontsize=fntSize)
+
+    fig.tight_layout()
+
+    # save figure
+    CCC = FE_plot
+    print(CCC)
+    plt.savefig(CCC,dpi=300,bbox_inches = "tight")
+    plt.close(fig)
+
+    end_code = time.perf_counter()
+    print('done creating dataset plot, elapsed time ' + str(end_code-start_code) + ' s')
--- scripts/python_utilities/coupler/couplingUtils.py
+++ scripts/python_utilities/coupler/couplingUtils.py
@@ -1,4 +1,8 @@
 import argparse
+import math
+import time
+from scipy.ndimage import gaussian_filter
+import numpy as np
 
 def parse_args():
     """ parse the command line arguments """
@@ -7,3 +11,83 @@ def parse_args():
     parser.add_argument("-f", "--file", required=True, help="JSON file with coupler parameter settings")
     args = parser.parse_args()
     return args
+
+def get_params_FE(FE_params_file):
+
+    FE_params_dict = {}
+    n_header = 1
+    f = open(FE_params_file,'r')
+    data = f.readlines()
+    f.close
+    row_len = len(data) - n_header
+    col_len = len(data[n_header].split())
+    for rr in range(n_header,row_len+n_header):
+        row_rr = data[rr]
+        if (row_rr[0]=='#'):
+            continue
+        varname_rr = row_rr.split('=')[0].split()
+        varval_rr = row_rr.split('=')[1].split('#')[0].split()
+        FE_params_dict[varname_rr[0]] = varval_rr
+
+    return FE_params_dict
+
+def zDeform(zRect, zGround, zCeiling, c1, fCoeff):
+
+    # FastEddy's vertical coordinate stretching formulation #
+    c2 = fCoeff*(1.0-c1)/zCeiling
+    c3 = (1.0-c2*zCeiling-c1)/(pow(zCeiling,2.0))
+    zStretch = (c3*pow(zRect,3.0)+c2*pow(zRect,2.0)+c1*zRect)*(zCeiling-zGround)/zCeiling+zGround
+
+    return zStretch
+
+def smoothTerrain(tPos0,dx):
+    start = time.time()
+    slopeThresh=math.tan(math.radians(35.0))
+    blend=0.2
+    n=3
+    gradTopoX,gradTopoY=np.gradient(tPos0,dx)
+    gradTopoR=np.sqrt(gradTopoX**2+gradTopoY**2)
+    max_dTdR=gradTopoR.max(axis=(0,1))
+    problematic_points0=np.sum(gradTopoR >= slopeThresh)
+    problematic_points=problematic_points0
+    iterSmooth=0
+    max_iter=problematic_points0*1.5
+    tPos1=tPos0.copy('K')
+    print(f'Problematic points: {problematic_points0}')
+    if (problematic_points0!=0):
+        while((max_dTdR>=slopeThresh) and (iterSmooth<=max_iter)):
+            sigTry = 25
+            tPos2=tPos1.copy('K')
+            problematic_points_solved = 0
+            while((max_dTdR>=slopeThresh) and (sigTry<=25) and (problematic_points_solved <= 0)):
+                pps = np.where(gradTopoR >= slopeThresh)
+                for pp in range(len(pps[0])):
+                    max_idx = (pps[0][pp], pps[1][pp])
+                    min_row = max(0, max_idx[0] - n//2)
+                    max_row = min(tPos1.shape[0], max_idx[0] + n//2+1)
+                    min_col = max(0, max_idx[1] - n//2)
+                    max_col = min(tPos1.shape[1], max_idx[1] + n//2+1)
+                    local_area = tPos1[min_row:max_row, min_col:max_col]
+                    local_blur_center = gaussian_filter(local_area, sigma=sigTry)
+                    for i in range(min_row, max_row):
+                        for j in range(min_col, max_col):
+                            if ((i, j) == max_idx):
+                                tPos2[i, j] = local_blur_center[i - min_row, j - min_col]
+                            else:
+                                tPos2[i, j] = (1-blend)*tPos1[i, j] + blend*local_blur_center[i - min_row, j - min_col]
+                gradTopoX_new, gradTopoY_new = np.gradient(tPos2, dx)
+                gradTopoR_new = np.sqrt(gradTopoX_new**2 + gradTopoY_new**2)
+                max_dTdR_new = gradTopoR_new.max(axis=(0, 1))
+                problematic_points_new=np.sum(gradTopoR_new >= slopeThresh)
+                problematic_points_solved=problematic_points - problematic_points_new
+                if((max_dTdR>=slopeThresh) and (problematic_points_solved <= 0)):
+                    sigTry+=1
+            iterSmooth+=1
+            problematic_points=problematic_points_new
+            tPos1=tPos2.copy('K')
+            gradTopoR = gradTopoR_new
+            max_dTdR = max_dTdR_new
+        end = time.time()
+        print(f'From {problematic_points0} to {problematic_points_new} prob points in {iterSmooth} iterations')
+        print(f'Elapsed time [s]: {np.round(end-start,3)}')
+    return tPos1
