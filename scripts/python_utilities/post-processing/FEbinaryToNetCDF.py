fbc513b79e12b0149e57537d64708f1521083d84
jprestop <jpresto@ucar.edu>
2025-08-21 13:30:53 -0600

    Per #88, initial attempt at code to add attributes to Python converter script

--- scripts/python_utilities/post-processing/FEbinaryToNetCDF.py
+++ scripts/python_utilities/post-processing/FEbinaryToNetCDF.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sysAA
 import struct
 import numpy as np
 import numpy.matlib
@@ -10,6 +10,7 @@ import gc
 import json
 import argparse
 from mpi4py import MPI
+from datetime import datetime
 
 def field3dTranspose(fld,extents):
     fld=fld.reshape(extents)
@@ -20,6 +21,223 @@ def field2dTranspose(fld,extents):
     fld=fld.reshape(extents)
     fldFinal=np.transpose(fld,axes=[1,0])
     return fldFinal[np.newaxis,Nh:-Nh,Nh:-Nh]
+
+def add_variable_attributes(ds):
+    """Add CF-compliant attributes to variables"""
+
+    # Define variable attributes based on common atmospheric variables
+    var_attrs = {
+        'u': {
+            'standard_name': 'eastward_wind',
+            'long_name': 'Eastward wind component',
+            'units': 'm s-1',
+            'description': 'Horizontal wind velocity component in the eastward direction'
+        },
+        'v': {
+            'standard_name': 'northward_wind',
+            'long_name': 'Northward wind component', 
+            'units': 'm s-1',
+            'description': 'Horizontal wind velocity component in the northward direction'
+        },
+        'w': {
+            'standard_name': 'upward_air_velocity',
+            'long_name': 'Vertical wind component',
+            'units': 'm s-1',
+            'description': 'Vertical wind velocity component'
+        },
+        'theta': {
+            'standard_name': 'air_potential_temperature',
+            'long_name': 'Potential temperature',
+            'units': 'K',
+            'description': 'Potential temperature of air'
+        },
+        'rho': {
+            'standard_name': 'air_density',
+            'long_name': 'Air density',
+            'units': 'kg m-3',
+            'description': 'Density of air'
+        },
+        'pressure': {
+            'standard_name': 'air_pressure',
+            'long_name': 'Air pressure',
+            'units': 'Pa',
+            'description': 'Atmospheric pressure'
+        },
+        'TKE_0': {
+            'long_name': 'Turbulent kinetic energy (resolved)',
+            'units': 'm2 s-2',
+            'description': 'Resolved-scale turbulent kinetic energy'
+        },
+        'TKE_1': {
+            'long_name': 'Turbulent kinetic energy (subgrid)',
+            'units': 'm2 s-2', 
+            'description': 'Subgrid-scale turbulent kinetic energy'
+        },
+        'qv': {
+            'standard_name': 'specific_humidity',
+            'long_name': 'Water vapor mixing ratio',
+            'units': 'kg kg-1',
+            'description': 'Mass mixing ratio of water vapor in air'
+        },
+        'ql': {
+            'standard_name': 'mass_fraction_of_cloud_liquid_water_in_air',
+            'long_name': 'Liquid water mixing ratio',
+            'units': 'kg kg-1',
+            'description': 'Mass mixing ratio of liquid water in air'
+        },
+        'qr': {
+            'long_name': 'Rain water mixing ratio',
+            'units': 'kg kg-1',
+            'description': 'Mass mixing ratio of rain water in air'
+        },
+        'xPos': {
+            'standard_name': 'projection_x_coordinate',
+            'long_name': 'X position coordinate',
+            'units': 'm',
+            'description': 'Physical x-coordinate position in meters'
+        },
+        'yPos': {
+            'standard_name': 'projection_y_coordinate', 
+            'long_name': 'Y position coordinate',
+            'units': 'm',
+            'description': 'Physical y-coordinate position in meters'
+        },
+        'zPos': {
+            'standard_name': 'height',
+            'long_name': 'Z position coordinate',
+            'units': 'm',
+            'description': 'Physical z-coordinate position (height) in meters'
+        },
+        'topoPos': {
+            'standard_name': 'surface_altitude',
+            'long_name': 'Topographic surface height',
+            'units': 'm', 
+            'description': 'Height of topographic surface above reference level'
+        },
+        'AuxScalar_0': {
+            'long_name': 'Auxiliary scalar field 0',
+            'units': 'dimensionless',
+            'description': 'User-defined auxiliary scalar variable (tracer or passive scalar)'
+        },
+        'AuxScalar_1': {
+            'long_name': 'Auxiliary scalar field 1', 
+            'units': 'dimensionless',
+            'description': 'User-defined auxiliary scalar variable (tracer or passive scalar)'
+        },
+        'tskin': {
+            'standard_name': 'surface_temperature',
+            'long_name': 'Skin temperature',
+            'units': 'K',
+            'description': 'Temperature at the surface skin layer'
+        },
+        'fricVel': {
+            'standard_name': 'friction_velocity',
+            'long_name': 'Friction velocity',
+            'units': 'm s-1',
+            'description': 'Surface friction velocity (u*)'
+        },
+        'htFlux': {
+            'standard_name': 'surface_upward_sensible_heat_flux',
+            'long_name': 'Surface sensible heat flux',
+            'units': 'W m-2',
+            'description': 'Upward sensible heat flux at the surface'
+        },
+        'invOblen': {
+            'long_name': 'Inverse Obukhov length',
+            'units': 'm-1', 
+            'description': 'Inverse of Obukhov length scale (1/L)'
+        },
+        'z0m': {
+            'standard_name': 'surface_roughness_length_for_momentum_in_air',
+            'long_name': 'Momentum roughness length',
+            'units': 'm',
+            'description': 'Surface roughness length for momentum transfer'
+        },
+        'z0t': {
+            'standard_name': 'surface_roughness_length_for_heat_in_air',
+            'long_name': 'Thermal roughness length',
+            'units': 'm',
+            'description': 'Surface roughness length for heat transfer'
+        }
+    }
+
+    # Apply attributes to variables
+    for var_name, var in ds.data_vars.items():
+        if var_name in var_attrs:
+            # Apply specific attributes for known variables
+            for attr_name, attr_value in var_attrs[var_name].items():
+                var.attrs[attr_name] = attr_value
+        else:
+            # Generic attributes for unknown variables
+            var.attrs['long_name'] = var_name.replace('_', ' ').title()
+            var.attrs['description'] = f'FastEddy model variable: {var_name}'
+
+            # Infer units based on variable name patterns
+            if any(x in var_name.lower() for x in ['temp', 'theta']):
+                var.attrs['units'] = 'K'
+            elif any(x in var_name.lower() for x in ['vel', 'wind', 'u', 'v', 'w']):
+                var.attrs['units'] = 'm s-1'
+            elif 'rho' in var_name.lower() or 'density' in var_name.lower():
+                var.attrs['units'] = 'kg m-3'
+            elif any(x in var_name.lower() for x in ['q', 'mixing', 'humidity']):
+                var.attrs['units'] = 'kg kg-1'
+            elif 'tke' in var_name.lower() or 'energy' in var_name.lower():
+                var.attrs['units'] = 'm2 s-2'
+            elif 'pressure' in var_name.lower():
+                var.attrs['units'] = 'Pa'
+            else:
+                var.attrs['units'] = 'unknown'
+            
+        # Add common attributes to all variables (handle both float and int types)
+        if var.dtype.kind == 'f':  # floating point
+            var.attrs['_FillValue'] = np.nan
+            var.attrs['missing_value'] = np.nan
+        elif var.dtype.kind == 'i':  # integer
+            var.attrs['_FillValue'] = -9999
+            var.attrs['missing_value'] = -9999
+        
+        # Add data type information
+        var.attrs['dtype'] = str(var.dtype)
+    
+    return ds
+
+def add_coordinate_attributes(ds):
+    """Add attributes to coordinate variables"""
+    
+    if 'time' in ds.coords:
+        ds['time'].attrs = {
+            'standard_name': 'time',
+            'long_name': 'time',
+            'axis': 'T'
+        }
+
+    coord_attrs = {
+        'xIndex': {
+            'standard_name': 'projection_x_coordinate',
+            'long_name': 'x-coordinate in Cartesian system',
+            'axis': 'X',
+            'units': 'index'
+        },
+        'yIndex': {
+            'standard_name': 'projection_y_coordinate', 
+            'long_name': 'y-coordinate in Cartesian system',
+            'axis': 'Y',
+            'units': 'index'
+        },
+        'zIndex': {
+            'standard_name': 'atmosphere_hybrid_height_coordinate',
+            'long_name': 'z-coordinate in Cartesian system',
+            'axis': 'Z',
+            'positive': 'up',
+            'units': 'index'
+        }
+    }
+
+    for coord_name, attrs in coord_attrs.items():
+        if coord_name in ds.coords:
+            ds[coord_name].attrs.update(attrs)
+    
+    return ds
 
 def readBinary(outpath,theseFiles):
     verboseLogging=False
@@ -77,6 +295,11 @@ def readBinary(outpath,theseFiles):
       
     #Concatenate all the perRank dataSets into a single dataset 
     dsFull=xr.concat(dsSet,'xIndex',data_vars='minimal')
+
+    # Add variable and coordinate attributes
+    dsFull = add_variable_attributes(dsFull)
+    dsFull = add_coordinate_attributes(dsFull)
+        
     return dsFull
 
 ###
