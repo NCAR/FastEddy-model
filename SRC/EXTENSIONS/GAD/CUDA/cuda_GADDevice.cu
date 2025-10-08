d826d37892e42b60a714a6ed85525f61f464556f
jprestop <jpresto@ucar.edu>
2025-10-08 12:34:32 -0600

    Per #89, adding license file to those files that don't have it

--- SRC/EXTENSIONS/GAD/CUDA/cuda_GADDevice.cu
+++ SRC/EXTENSIONS/GAD/CUDA/cuda_GADDevice.cu
@@ -1,3 +1,18 @@
+/* FastEddy®: SRC/EXTENSIONS/GAD/CUDA/cuda_GADDevice.cu
+* ©2016 University Corporation for Atmospheric Research
+* 
+* This file is licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 /*--- GAD*/ 
 __constant__ int GADSelector_d;     /* Generalized Actuator Disk Selector: 0=off, 1=on */
 __constant__ int GADoutputForces_d;   /* Flag to include GAD forces in the output: 0=off, 1=on */
--- SRC/HYDRO_CORE/CUDA/cuda_cellpertDevice.cu
+++ SRC/HYDRO_CORE/CUDA/cuda_cellpertDevice.cu
@@ -1,3 +1,18 @@
+/* FastEddy®: SRC/HYDRO_CORE/CUDA/cuda_cellpertDevice.cu
+* ©2016 University Corporation for Atmospheric Research
+* 
+* This file is licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 /*---CELL PERTURBATION METHOD*/
 __constant__ int cellpertSelector_d;    /*CP method selector: 0= off, 1= on */
 __constant__ int cellpert_sw2b_d;       /* switch to do: 0= all four lateral boundaries, 1= only south & west boundaries, 2= only south boundary */
