909e171dadf343b7c7ddcd5bc7919a8e27d04a12
willmayfield <wmayfield@ucar.edu>
2026-03-24 14:32:06 -0600

    Added json configuration for mpassit conversion script and renamed related input files

--- scripts/python_utilities/coupler/mpassit_to_fasteddy.py
+++ scripts/python_utilities/coupler/mpassit_to_fasteddy.py
@@ -1,22 +1,31 @@
 import os, sys
 import numpy as np
 import xarray as xr
+import argparse
+import json
 from netCDF4 import Dataset
 import datetime as dt
 import xarray as xr
 
+from couplingUtils import *
 
-dateString="2024-02-16"
-timeHour0=17
-timeMinute0=00
-timeSecond0=00
-secMax=7500
-secInc=300
-MPASSIT_PrntDir="./"
-MPASSIT_PrntOutPrefix="proc."
-FE_PrntOutPrefix="mpassit_"
+# Read run parameters from json
+args = parse_args()
+with open(args.file) as file:    
+    params = json.loads(file.read())    
+    
+MPASSIT_Dir = params["MPASSIT_Dir"]    
+MPASSIT_Prefix = params["MPASSIT_Prefix"]    
+FE_PrntOutDir = params["FE_PrntOutDir"]    
+FE_PrntOutPrefix = params["FE_PrntOutPrefix"]    
+dateString = params["date0"]    
+timeHour0 = params["timeHour0"]    
+timeMinute0 = params["timeMinute0"]    
+timeSecond0 = params["timeSecond0"]    
+secMax = params["secMax"]    
+secInc = params["secInc"]   
 
-#Define Constants
+# Define Constants
 grav=9.80665
 p1000mb=100000.0
 rv=461.6
@@ -26,9 +35,7 @@ cv=cp-rd
 rvovrd=rv/rd
 cvpm=-1.0*(cv/cp)
 
-################################################################################################    
-### Define a list of MPAS files to process from the specified coupler parameters    
-################################################################################################    
+# Define a list of MPAS files to process from the specified coupler parameters    
 files_list_mpassit=[]
 files_list_fe=[]
 
@@ -41,12 +48,13 @@ for it in range(0,secMax,secInc):
     dateString_it = str(date_it.year) + '-' + "{:02d}".format(date_it.month)  + '-' + "{:02d}".format(date_it.day) + '_'
     thistime_mpassit = "{:s}{:02d}.{:02d}.{:02d}".format(dateString_it,date_it.hour,date_it.minute,date_it.second)
     thistime_fe = "{:s}{:02d}:{:02d}:{:02d}".format(dateString_it,date_it.hour,date_it.minute,date_it.second)
-    file_tmp_mpassit = f'{MPASSIT_PrntDir}{MPASSIT_PrntOutPrefix}{thistime_mpassit}.nc'
-    file_tmp_fe = f'{MPASSIT_PrntDir}{FE_PrntOutPrefix}{thistime_fe}'
+    file_tmp_mpassit = f'{MPASSIT_Dir}{MPASSIT_Prefix}{thistime_mpassit}.nc'
+    file_tmp_fe = f'{FE_PrntOutDir}{FE_PrntOutPrefix}{thistime_fe}'
     files_list_mpassit.append(file_tmp_mpassit)
     files_list_fe.append(file_tmp_fe)
     date_it = date_it + dt.timedelta(seconds=secInc)
 
+# Perform a set of conversions on the new netcdf files
 for idx,file in enumerate(files_list_mpassit):
     print("Processing "+file)
     ds=xr.open_dataset(file)
