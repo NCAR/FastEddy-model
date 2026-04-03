a5c97564c16dfcbc671c750d7c474108a79f74cc
willmayfield <wmayfield@ucar.edu>
2026-04-03 12:22:35 -0600

    Changing forecast length to match files that will go into Zenodo record

--- scripts/batch_jobs/run_mpassit.sh
+++ scripts/batch_jobs/run_mpassit.sh
@@ -28,7 +28,7 @@
 
 start_init=20240216170000 # YYYYMMDDHHMMSS format
 diag_output_interval=300 #interval between lbc files in seconds
-FCST_RANGE=7200 #length of forecast in seconds
+FCST_RANGE=5400 #length of forecast in seconds
 MPAS_EXPT_DIR=/glade/derecho/scratch/wmayfield/dtc_ncar_mpas/expt_dirs/DTC_NCAR_hrrrIC/conus_3km_fasteddy/mpas_atm/2024021617/ens_1 #directory containing diag, history, init files 
 MPASSIT_CODE_DIR=/glade/campaign/ral/jntp/mayfield/fasteddy/MPASSIT #path to base MPASSIT code directory (contains ./bin/mpassit)
 VARLIST_DIR=/glade/campaign/ral/jntp/mayfield/fasteddy/run_mpassit/varlists_mpassit_fasteddy #directory containing the variable lists
